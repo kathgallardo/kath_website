cdfa369b67b660b83e0b54d9f6bfac47e932b80a
kathlynnG <Kathlynn.gallardo@paydro.ph>
2019-05-16 11:22:47 +0800

    vectors

--- tabs/icons.php
+++ tabs/icons.php
@@ -5,25 +5,13 @@
 		<img src="assets/icon/icon1.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
+		<img src="assets/icon/icon2.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
+		<img src="assets/icon/icon3.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
-	</figure>
-	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
-	</figure>
-	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
-	</figure>
-	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
-	</figure>
-	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b4/eb/e1/b4ebe19398773045b699fb4f4fa94164.jpg" class="-img">
+		<img src="assets/icon/icon4.png" class="-img">
 	</figure>
 </div>
 	
--- tabs/vector.php
+++ tabs/vector.php
@@ -2,28 +2,29 @@
 
 <div class="mobile">
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector5.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector1.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector6.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector2.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector3.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector4.png" class="-img">
+	</figure>
+
+	<figure class="image">
+		<img src="assets/vector/vector7.png" class="-img">
 	</figure>
 	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
-	</figure>
-	<figure class="image">
-		<img src="https://i.pinimg.com/564x/b3/87/73/b3877302fdd8ff82f691864bbfa57f5a.jpg" class="-img">
+		<img src="assets/vector/vector8.png" class="-img">
 	</figure>
 </div>
 	
